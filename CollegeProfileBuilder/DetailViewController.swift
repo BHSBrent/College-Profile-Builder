d66f43f715aa4b07f72f4f215cf62abb81fb5be9
Brent Behling <bbehling@Brents-MacBook-Air-2.local>
2017-02-13 10:13:49 -0600

    Added to the detailed storyboard

--- CollegeProfileBuilder/DetailViewController.swift
+++ CollegeProfileBuilder/DetailViewController.swift
@@ -10,18 +10,38 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var attendanceField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
 
-
+    var detailItem: Colleges? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+    
     func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
+        if let Colleges = self.detailItem {
+            if nameField != nil {
+                nameField.text = Colleges.name
+                locationField.text = Colleges.location
+                attendanceField.text = String(Colleges.attendance)
+                imageView.image = UIImage(data: Colleges.logo)
             }
         }
     }
 
+    @IBAction func onSaveTapped(_ sender: UIButton) {
+        if let Colleges = self.detailItem {
+            Colleges.name = nameField.text!
+            Colleges.location = locationField.text!
+            Colleges.attendance = Int(attendanceField.text!)!
+            Colleges.logo = UIImagePNGRepresentation(imageView.image!)!
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,14 +52,5 @@ class DetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
-    }
-
-
 }
 
